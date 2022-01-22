be99fdc71eceeb7465ef59849aa9bf3028805bf2
Eren Manis <emanis@travix.com>
2022-01-22 13:17:13 +0100

    Deprecate redundant method and improve the current one

--- helper/cerebro_helper.py
+++ helper/cerebro_helper.py
@@ -6,50 +6,6 @@ from helper.BackTraderIO import BackTraderIO
 logger = get_logger(__name__)
 tz = pytz.timezone("UTC")
 
-# TODO Deprecate this method and use the generic one for multiple timeframe loading
-def get_cerebro_with_filename(
-    filenames, strategy, cash=None, commission=None, stake=None
-):
-    cerebro = bt.Cerebro()
-    logger.debug("Cerebro created..")
-
-    if cash is not None:
-        cerebro.broker.setcash(cash)
-        logger.debug(f"Initial cash amount is set to {cash}")
-
-    if commission is not None:
-        cerebro.broker.setcommission(commission=commission)
-        logger.debug(f"Broker commission is set to {commission}")
-
-    if stake is not None:
-        cerebro.addsizer(bt.sizers.SizerFix, stake=stake)
-        logger.debug(f"Position size is fixed to {stake}")
-
-    try:
-        cerebro.addstrategy(strategy)
-        logger.debug(f"Strategy added to cerebro")
-    except Exception as e:
-        logger.error(f"Encountered an error trying to add strategy")
-        raise
-
-    try:
-        for filename in filenames:
-            # TODO This constructor won't work due to deleted overloaded __init__ in BackTraderIO
-            bt_data = BackTraderIO([filename]).get_all_data()
-            logger.debug(f"Data loaded from {filename}")
-
-            data = bt.feeds.PandasData(dataname=bt_data, tz=tz)
-
-            # TODO Add name parameter in case different symbols are present
-            cerebro.adddata(data)
-
-        cerebro.addtz(tz)
-        logger.debug(f"Data feed added to the simulation feed")
-    except Exception as e:
-        logger.error(f"Encountered an error trying to add data")
-
-    return cerebro
-
 
 def get_cerebro(
     symbol,
@@ -60,6 +16,7 @@ def get_cerebro(
     cash=None,
     commission=None,
     stake=None,
+    higher_intervals=[]
 ):
     cerebro = bt.Cerebro()
     logger.debug("Cerebro created..")
@@ -95,9 +52,21 @@ def get_cerebro(
             f"Data loaded from {fromdate} to {todate} for {symbol} per {interval}"
         )
 
+        # TODO Add data id since multiple dataframes can be added
         data = bt.feeds.PandasData(dataname=bt_data, tz=tz)
         cerebro.adddata(data)
+
+        # TODO TEST THIS
+        for higher_interval in higher_intervals:
+            bt_data = BackTraderIO(symbol, higher_interval, fromdate, todate).filter()
+            logger.debug(
+                f"Higher interval data loaded from {fromdate} to {todate} for {symbol} per {interval}"
+            )
+            data = bt.feeds.PandasData(dataname=bt_data, tz=tz)
+            cerebro.adddata(data)
+
         cerebro.addtz(tz)
+
         logger.debug(f"Data feed added to the simulation feed")
     except Exception as e:
         logger.error(f"Encountered an error trying to add data: {e}")
